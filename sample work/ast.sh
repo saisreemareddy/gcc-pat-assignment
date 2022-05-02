0a0fd2df1554c0a081fba350455e970d70d2dbc6
mahitha-reddy <mahithareddy.ls37@gmail.com>
2022-05-02 01:14:13 -0500

    commit

--- sample work/ast.sh
+++
@@ -1,1 +0,0 @@
-grep -EcoR "^[^#--/*//\"].*Assert.*\(.*\)" ./gcc/testsuite/* | grep -v :0 | wc -l >> numasserts.txt
--- sample work/numdbg.sh
+++
@@ -1,1 +0,0 @@
-grep -EcoR "^[^#--/*//\"].*Debug.*\(.*\)" ./gcc/testsuite/* | grep -v :0 | wc -l >> numdebug.txt
